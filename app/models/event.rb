7cdf991ce2ea9b1cccc1ac3d604640ceaaddb65a
Shintaro Tanaka <qpshinqp@mist-t.co.jp>
2018-01-10 15:19:06 +0900

    Util scope

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,9 @@
 class Event < ApplicationRecord
   has_one :leaderboard, :class_name => "EventLeaderboard"
   belongs_to :season
+  scope :ongoing,   -> { where(["open_at <= ? and close_at > ?", Time.zone.now, Time.zone.now]) }
+  scope :completed, -> { where(["close_at <= ?", Time.zone.now]) }
+  scope :scheduled, -> { where(["open_at > ?", Time.zone.now]) }
 
   def to_s
     self.leaderboard.to_s
