ed47649ceb9af91ed5ac054260339569b911864f
Shintaro Tanaka <qpshinqp@mist-t.co.jp>
2018-01-10 04:05:17 +0000

    Initialize bot only when Rails::Server is present

--- config/initializers/discord.rb
+++ config/initializers/discord.rb
@@ -1,68 +1,69 @@
 require 'discordrb'
 
-bot = Discordrb::Commands::CommandBot.new token: Rails.configuration.discord['token'], client_id: Rails.configuration.discord['client_id'], prefix: "/"
+if defined?(Rails::Server)
 
-bot.command(:register,
-            min_args: 0,
-            max_args: 2,
-            description: 'リーグをDiscordチャンネルに登録します。チャンネル名が "TeamID_LeagueName" の形式になっているのが好ましいですが、パラメータでも指定可能です',
-            usage: 'register [TeamId] [LeagueName]') do |event, team_id, league_name|
-  info = event.channel.name.split("_")
-  team_id     = info[0] unless team_id
-  league_name = info[1] unless league_name
+  bot = Discordrb::Commands::CommandBot.new token: Rails.configuration.discord['token'], client_id: Rails.configuration.discord['client_id'], prefix: "/"
 
-  league = League.find_or_create_by(team_id: team_id) do |l|
-    l.league_name = league_name
-  end
-  unless league.channels.exists?(channel_id: event.channel.id)
-    league.channels.build(channel_id: event.channel.id).save
-  end
-  SeasonUpdateJob.perform_later
-  SeasonStandingUpdateJob.perform_later
-  EventLeaderboardUpdateJob.perform_later
-  "Registered notification for #{league_name} league"
-end
+  bot.command(:register,
+              min_args: 0,
+              max_args: 2,
+              description: 'リーグをDiscordチャンネルに登録します。チャンネル名が "TeamID_LeagueName" の形式になっているのが好ましいですが、パラメータでも指定可能です',
+              usage: 'register [TeamId] [LeagueName]') do |event, team_id, league_name|
+    info = event.channel.name.split("_")
+    team_id     = info[0] unless team_id
+    league_name = info[1] unless league_name
 
-bot.command(:season,
-            min_args: 0,
-            max_args: 1,
-            description: 'シーズン情報を表示します',
-            usage: 'season [season_id]') do |event, season_id|
-  if season_id.present? and Season.exists?(season_id: season_id)
-    season = Season.find_by(season_id: season_id)
-    "#{season.open_at} 〜 #{season.close_at} シーズンのポイントランキング:\n" +
-    "```#{season.to_s}```"
-  elsif Channel.exists?(channel_id: event.channel.id)
-    begin
-      "```#{Channel.find_by(channel_id: event.channel.id).league.current_season.to_s}```"
-    rescue NoMethodError
-      "現在開催中のシーズンはありません"
+    league = League.find_or_create_by(team_id: team_id) do |l|
+      l.league_name = league_name
     end
-  else
-    "シーズン情報が見つかりませんでした。/registerコマンドでBotを登録してからイベント情報が更新されるまでに時間がかかる場合があります"
-  end
-end
-
-bot.command(:event,
-           min_args: 0,
-           max_args: 1,
-           description: 'イベント情報を表示します',
-           usage: 'event [event_id]') do |event, event_id|
-  if event_id.present? and Event.exists?(event_id: event_id)
-    event = Event.find_by(event_id: event_id)
-    "#{event.open_at} 〜 #{event.close_at} 開催の #{event.title} (#{event.countries}) の順位は\n" +
-    "```#{event.to_s}```"
-  elsif Channel.exists?(channel_id: event.channel.id)
-    begin
-      "```#{Channel.find_by(channel_id: event.channel.id).league.current_season.current_event.to_s}```"
-    rescue NoMethodError
-      "現在開催中のイベントはありません"
+    unless league.channels.exists?(channel_id: event.channel.id)
+      league.channels.build(channel_id: event.channel.id).save
     end
-  else
-    "イベント情報が見つかりませんでした。/registerコマンドでBotを登録してからイベント情報が更新されるまでに時間がかかる場合があります"
+    SeasonUpdateJob.perform_later
+    SeasonStandingUpdateJob.perform_later
+    EventLeaderboardUpdateJob.perform_later
+    "Registered notification for #{league_name} league"
   end
+
+  bot.command(:season,
+              min_args: 0,
+              max_args: 1,
+              description: 'シーズン情報を表示します',
+              usage: 'season [season_id]') do |event, season_id|
+    if season_id.present? and Season.exists?(season_id: season_id)
+      season = Season.find_by(season_id: season_id)
+      "#{season.open_at} 〜 #{season.close_at} シーズンのポイントランキング:\n" +
+      "```#{season.to_s}```"
+    elsif Channel.exists?(channel_id: event.channel.id)
+      begin
+        "```#{Channel.find_by(channel_id: event.channel.id).league.current_season.to_s}```"
+      rescue NoMethodError
+        "現在開催中のシーズンはありません"
+      end
+    else
+      "シーズン情報が見つかりませんでした。/registerコマンドでBotを登録してからイベント情報が更新されるまでに時間がかかる場合があります"
+    end
+  end
+
+  bot.command(:event,
+             min_args: 0,
+             max_args: 1,
+             description: 'イベント情報を表示します',
+             usage: 'event [event_id]') do |event, event_id|
+    if event_id.present? and Event.exists?(event_id: event_id)
+      event = Event.find_by(event_id: event_id)
+      "#{event.open_at} 〜 #{event.close_at} 開催の #{event.title} (#{event.countries}) の順位は\n" +
+      "```#{event.to_s}```"
+    elsif Channel.exists?(channel_id: event.channel.id)
+      begin
+        "```#{Channel.find_by(channel_id: event.channel.id).league.current_season.current_event.to_s}```"
+      rescue NoMethodError
+        "現在開催中のイベントはありません"
+      end
+    else
+      "イベント情報が見つかりませんでした。/registerコマンドでBotを登録してからイベント情報が更新されるまでに時間がかかる場合があります"
+    end
+  end
+
+  bot.run :async
 end
-
-bot.run :async
-
-# bot.send_message(channel_id, message)
