281974f97fa109df1b3c2f8fb1ffe1eb4651fcbc
Shintaro Tanaka <qpshinqp@mist-t.co.jp>
2018-11-20 17:06:14 +0900

    Fixed current_season selection logic

--- app/models/league.rb
+++ app/models/league.rb
@@ -2,7 +2,7 @@ class League < ApplicationRecord
   has_many :channels
   has_many :seasons
   def current_season
-    self.seasons.select {|e| e.open_at <= Time.now && Time.now <= e.close_at }.first
+    self.seasons.select {|e| e.open_at <= Time.now && Time.now <= e.close_at }.last
   end
   def next_season
     self.seasons.select {|e| Time.now < e.open_at }.first
